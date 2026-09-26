8a7cb8269bed24a15bedc864345d42263f3a9574
Jamie McCadden <madcadden@gmail.com>
2026-09-26 19:32:43 +0100

    Keep legacy fullscreen window handling unchanged in graphics-profile update

--- win32/Dll_Video.c
+++ win32/Dll_Video.c
@@ -108,7 +108,7 @@ static BOOL VIDEO_GoldenEyeMicrocode(unsigned int accessor)
     if(accessor > gAllocationLength || gAllocationLength - accessor < 24U)
         return FALSE;
     for(index = 0; index < 24U; index += 12U)
-        if((VIDEO_ROMWord(accessor + index) & 0xFFFF0000U) != 0x3C020000U ||
+        if((VIDEO_ROMWord(accessor + index) & 0xFFFF0000U) != 0x3C0200000U ||
             VIDEO_ROMWord(accessor + index + 4U) != 0x03E00008U ||
             (VIDEO_ROMWord(accessor + index + 8U) & 0xFFFF0000U) != 0x24420000U)
             return FALSE;
@@ -544,6 +544,7 @@ void VIDEO_ChangeWindow(int window)
 		EnableWindow((HWND)gui.hMenu1964main, TRUE);
 		ShowWindow(gui.hToolBar, SW_SHOW);
 		ShowWindow(gui.hStatusBar, SW_SHOW);
+		ShowWindow((HWND)gui.hMenu1964main, TRUE);
 		HideCursor(FALSE);
 		DockStatusBar();
 	}
